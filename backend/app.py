87cac5e757a8e9be0618424b03ad12000ec56a0b
Ofir <nofatigua@gmail.com>
2025-09-17 17:25:52 +0300

    make GraphQl work

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,34 @@
+import typing
+
+import datetime
+
 from contextlib import asynccontextmanager
 
+import strawberry
 from litestar.params import Dependency
 from typing_extensions import Any, AsyncGenerator
 from litestar import Litestar, get
 from litestar.di import  Provide
 from backend.controllers.item import ItemController
 from backend.db.config import engine, Base, get_db_session
+from strawberry.litestar import make_graphql_controller
+
+from litestar import Controller, post, get
+from litestar.params import Dependency
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql.schema import _InsertSentinelColumnDefault
+from sqlalchemy import ScalarResult
+import sqlalchemy
+from backend.schemes.item import ItemResponse, ItemCreate
+from typing import List
+
+from backend.db.shared import orm_to_dict
+from backend.models.item import TodoItemModel
+
+from backend.schema import Query, Mutation
+
+def custom_context_getter(db_session: Any=Dependency()):
+    return {'db_session': db_session}
 
 @asynccontextmanager
 async def db_lifespan(app) -> AsyncGenerator[None, None]:
@@ -16,13 +39,16 @@ async def db_lifespan(app) -> AsyncGenerator[None, None]:
 
     await engine.dispose()
 
-
 @get("/")
 async def index(db_session: Any = Dependency()) -> str:
     return "Hello, world!"
 
+schema = strawberry.Schema(query=Query, mutation=Mutation)
+
+graphql_contoller = make_graphql_controller(schema=schema, path="/graphql", context_getter=custom_context_getter)
+
 app = Litestar(
-    route_handlers=[index, ItemController],
+    route_handlers=[index, ItemController, graphql_contoller],
     dependencies={'db_session' : Provide(get_db_session)},
     lifespan=[db_lifespan],
     )
---
+++ backend/schema.py
@@ -0,0 +1,63 @@
+import datetime
+import strawberry
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import List
+import sqlalchemy
+from backend.models.item import TodoItemModel
+from sqlalchemy import ScalarResult
+import typing
+from backend.db.shared import orm_to_dict
+
+@strawberry.type
+class TodoItem:
+    id: int
+    text: str
+    completed: bool
+    created_at: datetime.datetime
+
+async def get_tasks(info: strawberry.Info) -> List[TodoItem]:
+    #, info: strawberry.Info[dict, None]
+    db_session: AsyncSession = info.context["db_session"]
+
+    tasks: List[TodoItem] = []
+    tasks_rows = sqlalchemy.select(TodoItemModel)
+
+    tasks_orm: ScalarResult = await db_session.scalars(tasks_rows)
+
+    for task in tasks_orm:
+        task_dict = orm_to_dict(task)
+        tasks.append(TodoItem(id=task_dict['id'],
+            text=task_dict['text'],
+            completed=task_dict['completed'],
+            created_at=task_dict['created_at']  )
+        )
+
+    #tasks = [TodoItem(text="1"), TodoItem(text="2")]
+
+    return tasks
+
+@strawberry.input
+class AddTaskInput:
+    text: str
+
+@strawberry.type
+class Query:
+    tasks: typing.List[TodoItem] = strawberry.field(resolver=get_tasks)
+
+@strawberry.type
+class Mutation:
+    #add_task: TodoItem = strawberry.mutation(resolver=add_task)
+    @strawberry.mutation
+    #info: strawberry.Info[dict, None]
+    async def add_task(self, info: strawberry.Info, text: str) -> TodoItem:
+        db_session: AsyncSession = info.context["db_session"]
+
+        new_item: TodoItemModel = TodoItemModel(text=text,
+            completed=False,
+            created_at=datetime.datetime.now())
+
+        db_session.add(new_item)
+        await db_session.commit()
+        await db_session.refresh(new_item)
+        #return TodoItem(id=1,text="123",completed=False,created_at=datetime.datetime.now())
+        return TodoItem(id=new_item.id, text=new_item.text, completed=new_item.completed, created_at=new_item.created_at)

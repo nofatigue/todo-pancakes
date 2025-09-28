e149fba2f17e292b68f15d18b6a7ec8eb2bd66ed
Ofir <nofatigua@gmail.com>
2025-09-28 14:45:23 +0300

    Added GraphQl query

---
+++ frontend/lib/backend.dart
@@ -0,0 +1,18 @@
+import 'package:frontend/graphql/__generated__/getTasks.req.gql.dart';
+import 'package:gql_http_link/gql_http_link.dart';
+import 'package:ferry/ferry.dart';
+import 'package:frontend/__generated__/schema.schema.gql.dart'
+    show possibleTypesMap;
+
+final link = HttpLink("http://localhost:8000/graphql");
+
+final cache = Cache(possibleTypes: possibleTypesMap);
+
+final client = Client(link: link, cache: cache);
+
+void doDbQuery() {
+  final getTasksReq = GgetTasksReq((b) => b);
+  client.request(getTasksReq).listen((response) {
+    print(response.data);
+  });
+}
